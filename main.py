0bbbae77838e610915ae5479ce54bec27bc2589b
alexarmez <alexa.ramirez0480@gmail.com>
2019-07-22 15:46:28 -0700

    Created the start page for my adventure game

---
+++ main.py
@@ -0,0 +1,16 @@
+import webapp2
+import jinja2
+import os
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+class MainHandle(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template('templates/start.html')
+        self.response.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandle),
+], debug=True)
